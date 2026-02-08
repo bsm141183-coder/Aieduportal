bf985e5c442d15cc9ea5cac6f5ffec49bbcef494
bsm141183-coder <bsm141183@gmail.com>
2026-02-08 20:57:02 +0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "aieduportal-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/Aieduportal/",
+        "/Aieduportal/index.html",
+        "/Aieduportal/manifest.webmanifest"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
